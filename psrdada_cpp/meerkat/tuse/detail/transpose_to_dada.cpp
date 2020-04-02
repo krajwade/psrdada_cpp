fa6f7afb2300a736f22e23afb39c40bad54269ed
kmrajwade <rkaustubh10@gmail.com>
2020-04-02 13:58:02 +0100

    Latest commit: changed the initial reserve on the buffers to resize

--- psrdada_cpp/meerkat/tuse/detail/transpose_to_dada.cpp
+++ psrdada_cpp/meerkat/tuse/detail/transpose_to_dada.cpp
@@ -40,10 +40,10 @@ namespace tuse {
 		std::uint32_t ii;
 		std::vector<std::thread> threads;
 		auto transpose_size = (_nchans *_nfreq/ _fscrunch) * (_nsamples * _ngroups/_tscrunch);
-        _transpose_buffers.reserve(_numbeams);
+        _transpose_buffers.resize(_numbeams);
         for (auto& buffer: _transpose_buffers)
         {
-            buffer.reserve(transpose_size);
+            buffer.resize(transpose_size);
         }
 
         bool thread_error = false;
